0514e441c1187edbcfcd51bbafdd074f36cd526b
Ofentse Seko <ofentse.seko21@gmail.com>
2026-05-18 15:28:31 +0200

    Add internal staff creation feature

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
-urlpatterns=[
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('staff/', views.staff_list, name='staff_list'),
+
+urlpatterns = [
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("staff/", views.staff_list, name="staff_list"),
+    path("staff/new/", views.staff_create, name="staff_create"),
 ]
